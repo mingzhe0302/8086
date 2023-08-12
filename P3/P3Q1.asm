05bd0240cfcaa6b47f27da11616eb1cc6daa14fa
fakeryeoh <yeohmz-wm22@student.tarc.edu.my>
2023-08-13 01:01:57 +0800

    new

--- P3/P3Q1.asm
+++ P3/P3Q1.asm
@@ -3,7 +3,7 @@
 .data
 	
     data1 db 'MILK', '$'
-    data2 db '****', '$'
+    data2 db 4 dup ('*'), '$'
 
     var1 db 'data1: ', '$'
     var2 db 'data2: ', '$'
@@ -11,7 +11,7 @@
     var3 db 'Initial content', '$'
     var4 db 'After Replacement', '$'
 
-	
+	len db 04h
 .code
 
 
@@ -20,6 +20,10 @@ MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
 
+    lea SI, data1
+    lea DI, data2
+
+
     ;display initial content
     lea dx, var3
     mov ah, 09h
@@ -27,11 +31,55 @@ MAIN PROC
     
     call NEWLINE
 
+    ;print "data1: MILK"
     mov ah, 09h
     lea dx, var1
     int 21H
 
     lea dx, data1
+    int 21H
+
+    call NEWLINE
+
+    ;print "data2: ****"
+    mov ah, 09h
+    lea dx, var2
+    int 21H
+
+    lea dx, data2
+    int 21h
+
+
+    call NEWLINE
+    call NEWLINE
+
+    ;display after Replacement value
+    lea dx, var4
+    mov ah, 09h
+    int 21H
+
+    call NEWLINE
+    
+    mov ah, 09h
+    lea dx, var1
+    int 21H
+
+    lea dx, data1
+    int 21H
+
+    call NEWLINE
+
+    mov ah, 09h
+    lea dx, var2
+    int 21H
+
+    CLD         ;clear direction flag
+    mov ch, 00h
+    mov cl, len
+    rep movsb
+
+    mov ah, 09h
+    lea dx, data2
     int 21H
 
 	;exit
@@ -48,7 +96,8 @@ NEWLINE PROC
     int 21H
 
     ret         ;return to the calling mode
-    
+
 NEWLINE ENDP
-                                       
+
+
 END MAIN
